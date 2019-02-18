8b0e8a66b06b4e8eb9a7975b392fa33fa9376d0b
Deepak Kumar Jha <deepak.kumar@effone.com>
2019-02-18 16:44:34 +0530

    heroku deployment config updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://sweeten-matching-algo.herokuapp.com/' }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  config.serve_static_assets = true
+  config.assets.digest = true
 end
